9f10b2e8c788430d0a8079fa553538a28f237675
Nikita Martyanov <nikita.martyanov@gmail.com>
2021-03-20 19:50:52 +0300

    added file upload function

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var multer  = require('multer');
 var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+var upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +13,13 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({
+    name :req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 
